475f2829ac55c1c15684f44d3cdb5e3c1fb9544b
wir0x <gsalazar-v@outlook.com>
2014-10-26 20:56:51 -0400

    Change in the Windows Form1

--- Sistema/Presentacion/Form1.cs
+++ Sistema/Presentacion/Form1.cs
@@ -16,5 +16,10 @@ namespace Presentacion
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello wordl");
+        }
     }
 }
